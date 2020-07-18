32cc42ac94551591ed4f638e260ae4386d5dbdc9
AJITESH3210 <jalluri3210@gmail.com>
2020-07-18 08:54:53 +0530

    #Files added

--- sketch.js
+++ sketch.js
@@ -6,17 +6,20 @@ var dog_Img;
 
 function preload()
 {
-  dog = loadImage("../images/Dog.png");
-  happyDog = loadImage("../images/happydog.png");
-  dogHappy = loadImage("../images/happydog.png");
+  dog_Img = loadImage("../images/Dog.png");
+  happyDog_Img = loadImage("../images/happydog.png");
+  dogHappy_Img = loadImage("../images/happydog.png");
 }
 //car2_img = loadImage("../images/car2.png");
 function setup() {
   createCanvas(500, 500);
   database = firebase.database();
 
-  var dog = createSprite(250,250,30,30);
-  dog.addImage("image/Dog.png", dog_Img)
+  var dog = createSprite(250, 250, 10, 10);
+  
+  dog.addImage("image/Dog.png", dog_Img);
+  
+
 
   foodStock = database.ref('Food');
   foodStock.on("value", readStock);
@@ -26,13 +29,18 @@ function setup() {
 
 function draw() {  
   background(46, 139, 87);
-
+  
   if(keyWentDown(UP_ARROW)){
     writeStock(foodS);
     dog.addImage(dogHappy)
   }
+  
   drawSprites();
-  //add styles here
+  
+  textSize(15);
+  fill("yellow");
+  stroke(10);
+  text("Note: Press UP_ARROW key to feed milk to your dog.", 80,480);
 
 }
 
@@ -57,8 +65,3 @@ function writeStock(x){
 
 
 
-/*
-
-car1 = createSprite(100,200);
-car1.addImage("car1",car1_img);
-*/
